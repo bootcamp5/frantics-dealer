0c7ef947562332c0aa4960d23d8afa137f182f5d
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2020-06-10 22:13:20 +0200

    add actioncard service and component

---
+++ src/app/action-card.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActionCardService {
+
+  constructor() { }
+
+  getCards(): ActionCardItem[] {
+    return [
+      {
+        name: 'russian-roulette',
+        title: 'Russian Roulette',
+        description: 'foo bar'
+      },
+      {
+        name: 'communism',
+        title: 'Communism',
+        description: 'Kommunismus!!'
+      },
+    ];
+  }
+
+}
+
+
+export interface ActionCardItem {
+  name: string;
+  title: string;
+  description: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionCardService } from './action-card.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frantics-dealer';
+  constructor(private actionsCards: ActionCardService) {  }
+
+  getItems() {
+    return this.actionsCards.getCards();
+  }
 }
